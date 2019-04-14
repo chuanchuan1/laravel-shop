fa5b366f7747e3da935af82effc79c0ff57ab457
vagrant <vagrant@homestead>
2019-04-14 03:54:43 +0000

    基础布局

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield("title", "laravel-shop") - Laravel 电商教程</title>
+    {{-- 样式 --}}
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield("content")
+        </div>
+        @include("layouts._footer")
+    </div>
+    {{-- JS 脚本 --}}
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,9 @@ const mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
+ | 在末尾加了 version() 方法，使 Mix 每次生成的静态文件后面加上一个类似版本号的参数，避免浏览器缓存
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .version();
