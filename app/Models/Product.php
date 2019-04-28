15741fc191cdc1362fb6f35b0d76900867449a1f
chuanchuan1 <1126371945@qq.com>
2019-04-28 02:43:32 +0000

    商品模型

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'image', 'on_sale',
+        'rating', 'sold_count', 'review_count', 'price'
+    ];
+
+    protected $casts = [
+        'on_sale' => 'boolean'
+    ];
+
+    // 与商品 SKU 关联
+    public function skus()
+    {
+        return $this->hasMany("ProductSku");
+    }
+}
